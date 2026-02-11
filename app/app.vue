e632fde76d4216e6d72dd2803593a88c9ebcf333
Gandoki <abahgideon111@gmail.com>
2026-02-11 09:54:07 +0100

    I fix somethnig i dont want you to know hahahaha

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  ]
+})
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
